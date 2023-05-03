2a672efabebc8626df3e625f3f9048cbd027fb19
Emre Ergül <emredevsalot@gmail.com>
2023-05-03 05:52:59 +0300

    add Homepage

---
+++ src/pages/homepage/HomepageRow.tsx
@@ -0,0 +1,66 @@
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+
+const variantsHomepageRow = {
+  initial: {
+    y: 30,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.8, ease: "easeInOut" },
+  },
+  exit: {
+    opacity: 0,
+    transition: { ease: "easeInOut" },
+  },
+};
+
+type Props = {
+  index: number;
+  title: string;
+  description: string;
+  image: string;
+};
+
+const HomepageRow = ({ index, title, description, image }: Props) => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  useEffect(() => {
+    if (inView) {
+      controls.start("animate");
+    }
+    // if (!inView) {
+    //   controls.start('exit');
+    // }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={variantsHomepageRow}
+      initial="initial"
+      animate={controls}
+      className={`home2__row ${index % 2 === 0 ? "" : "reverse"} ${
+        index !== 0 ? "borderTop" : ""
+      }`}
+    >
+      <div
+        className="home2__row__icon"
+        style={{
+          backgroundImage: image,
+        }}
+      ></div>
+      <div className="home2__row__info ml-5">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default HomepageRow;
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,41 +1,108 @@
-import { useAnimate, usePresence } from "framer-motion";
-import { useEffect } from "react";
+import { useRef, useEffect } from "react";
+
+import { motion } from "framer-motion";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+import { homepageRowData } from "../../data";
+import Button from "../../components/Button";
+import HomepageRow from "./HomepageRow";
+
+const variantsHero = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 1.5, delay: 0.5 }, //ease: [0.6, 0.01, -0.05, 0.9] },
+  },
+  exit: {
+    opacity: 0,
+    transition: { delay: 0.5 }, //ease: [0.6, 0.01, -0.05, 0.9] },
+  },
+};
+const variantsHeroButton = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 1.5, delay: 1.3 }, //ease: [0.6, 0.01, -0.05, 0.9] },
+  },
+
+  exit: {
+    opacity: 0,
+    transition: { delay: 0.5 }, //ease: [0.6, 0.01, -0.05, 0.9] },
+  },
+};
 
 const Homepage = () => {
-  const [isPresent, safeToRemove] = usePresence();
-  const [scope, animate] = useAnimate();
-
+  const developmentSection = useRef<any>(null);
+  const goToDevelopmentSection = () => {
+    if (developmentSection.current == null) return;
+    window.scrollTo({
+      top: developmentSection.current.offsetTop - 260,
+      behavior: "smooth",
+    });
+  };
   useEffect(() => {
-    if (isPresent) {
-      const enterAnimation = async () => {
-        await animate("h1", {
-          opacity: [0, 1],
-          transform: ["translateY(100px)", "translateY(0px)"],
-        });
-      };
-      enterAnimation();
-      console.log("enterAnimation");
-    } else {
-      const exitAnimation = async () => {
-        await animate("h1", {
-          opacity: [1, 0],
-          transform: ["translateY(0px)", "translateY(-100px)"],
-        });
-        safeToRemove();
-        console.log("safeToRemove");
-      };
-      exitAnimation();
-      console.log("exitAnimation");
-    }
-  }, [isPresent]);
-
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <div
-      className="h-screen flex items-center justify-center bg-slate-200"
-      ref={scope}
-    >
-      <h1 className="text-3xl">Hello world</h1>
-    </div>
+    <>
+      <motion.div
+        variants={variantsHero}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="hero"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 30%), url('/assets/hero-image.jpg')`,
+        }}
+      >
+        <motion.div
+          variants={variantsHeroButton}
+          whileHover={{
+            y: -10,
+            transition: { ease: "easeInOut" },
+          }}
+          className="hero__button"
+          onClick={goToDevelopmentSection}
+        >
+          <div className="hero__text">
+            <h3>How we develop our projects</h3>
+          </div>
+          <div className="hero__icon">
+            <FontAwesomeIcon icon={faChevronDown} color="white" size="2x" />
+          </div>
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        variants={variantsHero}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="home2"
+      >
+        <div className="container">
+          <div ref={developmentSection}></div>
+          {homepageRowData.map((data, index) => (
+            <HomepageRow
+              index={index}
+              title={data.title}
+              description={data.description}
+              image={data.image}
+            />
+          ))}
+          <div className="home2__row borderTop">
+            <Button url="/projects" text="Our Projects" />
+          </div>
+        </div>
+      </motion.div>
+      {/* <Footer /> */}
+    </>
   );
 };
 
