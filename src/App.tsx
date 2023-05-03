943e1b292874c568a6a5e050717439f533568484
Emre Ergül <emredevsalot@gmail.com>
2023-05-03 05:53:46 +0300

    rename Services to Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { useLocation, useRoutes } from "react-router-dom";
+import "../src/sass/main.scss";
 import { AnimatePresence } from "framer-motion";
 
 import Homepage from "./pages/homepage";
-import Services from "./pages/services";
+import Projects from "./pages/projects";
 
 function App() {
-  //EX: https://codesandbox.io/s/framer-motion-react-router-6-page-transitions-2f2olf
-
   const routes = useRoutes([
     {
       path: "/",
       element: <Homepage />,
     },
     {
-      path: "/services",
-      element: <Services />,
+      path: "/projects",
+      element: <Projects />,
     },
   ]);
 
