df5ae7b4e793f51d49297f21adcbbd858d5b6897
Emre Ergül <emredevsalot@gmail.com>
2023-05-27 18:57:23 +0300

    update AnimatedTitle

--- src/components/AnimatedTitle.tsx
+++ src/components/AnimatedTitle.tsx
@@ -8,20 +8,22 @@ type Props = {
 
 const AnimatedTitle = ({ title }: Props) => {
   return (
-    <h1 className="animatedTitle">
-      <motion.span
-        variants={variantsStaggerParent()}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-      >
-        {title.split("").map((letter) => (
-          <motion.span variants={variantsSlideInChild({ initY: 350 })}>
-            {letter !== " " ? letter : <>&nbsp;</>}
-          </motion.span>
-        ))}
-      </motion.span>
-    </h1>
+    <div className="animatedTitleContainer">
+      <h1 className="animatedTitle">
+        <motion.span
+          variants={variantsStaggerParent()}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          {title.split("").map((letter) => (
+            <motion.span variants={variantsSlideInChild({ initY: 350 })}>
+              {letter !== " " ? letter : <>&nbsp;</>}
+            </motion.span>
+          ))}
+        </motion.span>
+      </h1>
+    </div>
   );
 };
 
