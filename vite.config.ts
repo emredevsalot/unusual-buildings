c4b1e8bf6c9626ccd78ddb22f90a8dc6747ddc64
Emre Ergül <emredevsalot@gmail.com>
2023-06-01 23:37:07 +0300

    fix: add base to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,5 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  base: "/unusual-buildings/",
 });
